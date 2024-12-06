a1a4ae8f8636f187c506e35b5eb56d62ae99c952
Jessie Wang <jmw9952@nyu.edu>
2024-12-06 01:15:32 -0500

    12/5 last push

--- index.js
+++ index.js
@@ -7,10 +7,3 @@ app.use('/', express.static('public'));
 app.listen(3000, ()=> {
     console.log("app is listening at localhost:3000");
 });
-//Initialize the actual HTTP server
-// let https = require('https');
-// let server = https.createServer(app);
-// let port = process.env.PORT || 3000;
-// server.listen(port, () => {
-//     console.log("Server listening at port: " + port);
-// });
